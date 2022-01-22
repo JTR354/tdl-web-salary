58fc80dd3aa0d64385385e59e5ad440d62b1684c
jtr354 <jtr354@foxmail.com>
2022-01-22 09:03:45 +0800

    feat: [js 类型转换 注释]

--- src/js/01-类型转换.spec.js
+++ src/js/01-类型转换.spec.js
@@ -1,6 +1,12 @@
 const { f1 } = require("./01-类型转换");
 describe("类型转换", () => {
   test("should first", () => {
+    /**
+     * 运算符优先级 ():21
+     * + 隐式转为数字类型
+     * js 数据类型:
+     * string number boolean function object undefined
+     */
     expect(+f1()).toBe(0);
     expect(+f1()()).toBe(1);
     expect(+f1()()()).toBe(2);
